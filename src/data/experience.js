2ceda819726a5567290128a4350c2970d960bccf
Barath <contactmebarath@gmail.com>
2026-06-13 22:15:16 +0530

    Changes made

--- src/data/experience.js
+++ src/data/experience.js
@@ -6,10 +6,8 @@ export const experience = [
     duration: 'May 2026 – June 2026',
     type: 'Internship',
     achievements: [
-      'Worked on Data Science and Analytics projects (',
+      'Worked on Data Science and Analytics projects (Online Retail Sales Analysis, Customer Churn Analysis, Marketing Funnel and Conversion Performance Analysis)',
       'Strengthened my skills in Python, Power BI, data analysis, and data visualization while gaining valuable hands-on experience',
-      'Led migration of monolith to 12 microservices, cutting deploy time by 70%',
-      'Mentored 3 junior engineers; established code review guidelines adopted org-wide',
     ],
   },
 ];
