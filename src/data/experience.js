6ebd5128244d2019bd760d987856a140fc405510
Barath <contactmebarath@gmail.com>
2026-08-04 23:47:00 +0530

    Adding patent under experience

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,17 +1,6 @@
 export const experience = [
   {
     id: 1,
-    org: 'Future Interns',
-    role: 'Data Science and Analytics Intern',
-    duration: 'May 2026 – June 2026',
-    type: 'Internship',
-    achievements: [
-      'Worked on Data Science and Analytics projects (Online Retail Sales Analysis, Customer Churn Analysis, Marketing Funnel and Conversion Performance Analysis)',
-      'Strengthened my skills in Python, Power BI, data analysis, and data visualization while gaining valuable hands-on experience',
-    ],
-  },
-  {
-    id: 2,
     org: 'BlackVolt Technologies Pvt. Ltd.',
     role: 'Artificial Intelligence and Machine Learning Intern',
     duration: 'June 2026 - Present',
@@ -19,6 +8,17 @@ export const experience = [
     achievements: [
       'Currently working as an AI/ML Intern, gaining industry exposure to Artificial Intelligence and Machine Learning',
       'Enhancing my technical skills through practical learning, collaboration, and real-world development experience',
+    ],
+  },
+  {
+    id: 2,
+    org: 'Unified Project Management System',
+    role: 'Co-inventor (Published Indian Patent)',
+    duration: 'June 2025 - Present',
+    type: 'Patent',
+    achievements: [
+      'Engineered a unified project management platform designed to streamline team formation, guide allocation, project submissions, and evaluation workflows through a centralized system',
+      'Designed a randomized panel-assignment algorithm preventing duplicate guide/panel allocation, ensuring fair, conflict-free evaluations.',
     ],
   },
   {
@@ -32,5 +32,15 @@ export const experience = [
       'Engaging in coding activities, collaborative events, and continuous learning to strengthen problem-solving skills',
     ],
   },
-  
+  {
+    id: 4,
+    org: 'Future Interns',
+    role: 'Data Science and Analytics Intern',
+    duration: 'May 2026 – June 2026',
+    type: 'Internship',
+    achievements: [
+      'Worked on Data Science and Analytics projects (Online Retail Sales Analysis, Customer Churn Analysis, Marketing Funnel and Conversion Performance Analysis)',
+      'Strengthened my skills in Python, Power BI, data analysis, and data visualization while gaining valuable hands-on experience',
+    ],
+  },
 ];
